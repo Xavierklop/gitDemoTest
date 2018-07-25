a77e5fcd467328d7b8786b00552443f26a16add1
Hao Wu <haowu@haos-macbook-pro.local>
2018-07-25 15:16:49 +0200

    Initial Commit

---
+++ getGitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getGitDemo
+//
+//  Created by Hao Wu on 2018/7/25.
+//  Copyright © 2018年 Hao Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
